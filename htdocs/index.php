0288778a6858d8b5153b4796d719ecbf728cbab7
7074524287 <singhasouvik823@gmail.com>
2024-08-03 21:03:22 +0530

    V1.0.0

---
+++ htdocs/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xporn</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet"href="style.css">
+</head>
+<body>
+   <header>
+    <div class="navbar">
+        <h1>XPORN</h1>
+<div class="panel-all">
+   <a href="menu.php"> <i class="fa-solid fa-bars"></i></a>
+</div>
+    </div>
+   </header>
+</body>
+</body>
+</html>
